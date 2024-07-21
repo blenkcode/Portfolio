ec57d6a66f27eb347d1c8f40bd3de5ddb99f566d
Your name <Your email>
2024-07-21 17:11:15 +0200

    updatefiles

--- components/Works.js
+++ components/Works.js
@@ -76,7 +76,7 @@ function Works() {
                         controls
                         ref={videoRef}
                       >
-                        <source src={"/Carbydmo.mp4"} type="video/mp4" />
+                        <source src={"/carbydemo2.mp4"} type="video/mp4" />
                         Your browser does not support the video tag.
                       </video>
                     </div>
